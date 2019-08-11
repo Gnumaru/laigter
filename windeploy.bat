8b077f8e46852eb1f20d3041ebce794aab5166c0
LucasFernandoCarlos <lucas.fernando@ufla.br>
2019-08-11 16:41:04 -0300

    .

--- windeploy.bat
+++ windeploy.bat
@@ -1,6 +1,10 @@
 :: make sure that C:\Qt\5.13.0\mingw73_32\bin is on path
 
-windeployqt --dir .\deploy ..\build-laigter-Desktop_Qt_5_13_0_MinGW_32_bit-Release\release\laigter.exe
+rd /q /s .\deploy
+
+mkdir .\deploy
+
+C:\Qt\5.13.0\mingw73_32\bin\windeployqt.exe --dir .\deploy ..\build-laigter-Desktop_Qt_5_13_0_MinGW_32_bit-Release\release\laigter.exe
 
 copy /Y ..\build-laigter-Desktop_Qt_5_13_0_MinGW_32_bit-Release\release\laigter.exe .\deploy\laigter.exe
 
